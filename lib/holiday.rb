55c052b02be95690ec80d40794490a89fcbadbf5
Adam Urbina <adamurbina@gmail.com>
2018-06-02 01:24:29 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each { |season, holiday|
     puts season.to_s.capitalize + ":"
+    modified_holiday = holiday.to_s["_"] = " "
 }
 
 end
