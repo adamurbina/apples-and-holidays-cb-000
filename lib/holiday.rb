e2ae68a41435cfc393e92aa50057708c076b4004
Adam Urbina <adamurbina@gmail.com>
2018-06-02 01:44:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
         holiday_array = holiday.to_s.split("_")
         holiday_array.each{|holiday| holiday.capitalize}
         puts holiday_array
-        modified_holiday =
+        modified_holiday = holiday_array.join(" ")
         puts (modified_holiday + ":").chomp!
         puts supplies
 
