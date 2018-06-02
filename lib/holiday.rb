ee0bdf35d17703bb4a4b89601e1762f9ee74e6a7
Adam Urbina <adamurbina@gmail.com>
2018-06-02 01:59:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,5 +78,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holidays = []
 
+  holiday_hash.each{ |season, holiday_array|
+    }
 end
