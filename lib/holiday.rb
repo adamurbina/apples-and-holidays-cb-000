bccc0d8171dde9f64520b55935596ad5390b4258
Adam Urbina <adamurbina@gmail.com>
2018-06-02 01:48:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
         holiday_mod = holiday.to_s.split("_")
         holiday_mod.each{|day| day.capitalize!}
         modified_holiday = holiday_mod.join(" ")
-        puts (modified_holiday + ":").chomp
+        puts (modified_holiday + ":")
         #puts supplies
 
     }
