a1e9807ff31809ce03adeb095116f462fab5fb9a
Adam Urbina <adamurbina@gmail.com>
2018-06-02 01:49:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,12 +61,12 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize + ":"
 
     holiday_array.each { |holiday, supplies|
-        
+
         holiday_mod = holiday.to_s.split("_")
         holiday_mod.each{|day| day.capitalize!}
-        modified_holiday = holiday_mod.join(" ")
-        
-        puts (modified_holiday + ":" )
+        holiday_string = holiday_mod.join(" ")
+
+        puts (holiday_string + ":" )
     }
 
 }
